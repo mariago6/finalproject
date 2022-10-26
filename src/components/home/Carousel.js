6cc8f3019e107d5b37677facc9b6486bf832a025
Maria Go <merigon_93@hotmail.com>
2022-10-26 18:01:22 +0200

    improve carousel

--- src/components/home/Carousel.js
+++ src/components/home/Carousel.js
@@ -6,44 +6,47 @@ import '../../custom.scss';
 function CarouselHome({index, handleSelect}) {
 
   return (
-      <Carousel activeIndex={index} onSelect={handleSelect}>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="./images/salad.jpg"
-            alt="First slide"
-          />
-          <Carousel.Caption>
-            <h1 className='mb-4'>Choose the ingredients</h1>
-            <hr />
-            <h5>Cook with the ingredients you have in your kitchen</h5>            
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="./images/pasta.jpg"
-            alt="Second slide"
-          />
-          <Carousel.Caption>
-            <h1 className='mb-4'>Are you vegetarian/vegan?</h1>
-            <hr />
-            <h5>A lot of recipies without meat!</h5>            
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="./images/chilly.jpg"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h1 className='mb-4'>Do you have a intolerance?</h1>
-            <hr />
-            <h5>Find the perfect recipies to feel good every dish</h5>            
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
+      <Carousel variant="light" activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/salad.jpg"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <div className='containerCarousel py-3'>
+            <h2>Choose the ingredients</h2>
+            <p>Cook with the ingredients you have in your kitchen</p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/pasta.jpg"
+          alt="Second slide"
+        />
+        <Carousel.Caption>
+          <div className='containerCarousel py-3'>
+            <h2>Are you vegetarian or vegan?</h2>
+            <p>A lot of recipies without meat!</p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="./images/chilly.jpg"
+          alt="Third slide"
+        />
+        <Carousel.Caption>
+          <div className='containerCarousel py-3'>
+            <h2>Do you have any intolerance?</h2>
+            <p> Find the best recipe to feel good at all meals </p>
+          </div>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   
   );
 }
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 import {BsLinkedin, BsFacebook, BsInstagram, BsTwitter} from 'react-icons/bs';
 
 function Footer() {
+  const date = new Date();
+  const year = date.getFullYear();
+
   return(
     <div className='footer bg-primary text-center'>
       <div className='container'>
@@ -18,39 +18,11 @@ function Footer() {
         </div>
         <div className='row'>
           <div className='col-12'>
-            <p><small>@2022 copyright</small></p>
+            <p><small>@{year} All Rights Reserved</small></p>
           </div>
         </div>
       </div>
     </div>
-    // <div>
-    //   <Navbar collapseOnSelect expand="lg" bg="primary" variant="light">
-    //   {/* {isLoading && <Loader />} */}
-    //     <Container>
-    //       {/* <Navbar.Brand href="/">Recipes</Navbar.Brand> */}
-    //       <Navbar.Toggle  />
-    //       <Navbar.Collapse id="responsive-navbar-nav" >
-    //         <Nav className="m-auto justify-content-center col-5" >
-    //           <BsLinkedin className='m-2'/>
-    //           <BsFacebook className='m-2'/>
-    //           <BsInstagram className='m-2'/>
-    //           <BsTwitter className='m-2'/>
-    //         </Nav>
-    //       </Navbar.Collapse>
-    //     </Container>
-    //   </Navbar>
-    //   <Navbar collapseOnSelect expand="lg" bg="primary" variant="light">
-    //   {/* {isLoading && <Loader />} */}
-    //     <Container>
-    //       {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-    //       <Navbar.Collapse id="responsive-navbar-nav" >
-    //         <Nav className="m-auto justify-content-center" > 
-    //           <p className='col-5 text-center'>kdfgldfgdlgmdlk mgjkdlfmglkdfm gklfdkgldfglfd lgkjfgkdj flkgfdl gmflgdmflklkg lgmldgmdfgmd klbvbc vbf hfgh fgh ghgjg hg bnbnv vbnvnv bnvbn vbn bnbn bnbf dhbm lvll</p>
-    //         </Nav>
-    //       </Navbar.Collapse>
-    //     </Container>
-    //   </Navbar>
-    // </div>
   )
 }
 
